76738dea81cd7f9e9dd6cecea8c166aec2a0847e
Chandegaraa <chandegaraa@gmail.com>
2025-06-14 12:05:31 +0530

    Login.servlet.java

---
+++ Login.servlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogInServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            Connection con = DBConnection.initializeDatabase();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username=? AND password=?");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                response.sendRedirect("welcome.jsp");
+            } else {
+                response.sendRedirect("login.jsp?error=Invalid username or password");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
